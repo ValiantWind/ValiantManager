5efef2fb4ad5c15af9528c15bfe9f63caead79e5
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2022-01-28 18:43:39 -0800

    e

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,44 +1,44 @@
-const Discord = require('discord.js');
-const Levels = require('discord-xp')
-const canvacord = require('canvacord')
-module.exports = {
-    name: 'rank',
-    run: async (client, message, args) => {
-        const target = message.mentions.users.first() || message.author
-        let memberTarget = message.guild.members.cache.get(target.id);
-        const user = await Levels.fetch(target.id, message.guild.id, true);
-        const neededXp = Levels.xpFor(parseInt(user.level) + 1);
+// const Discord = require('discord.js');
+// const Levels = require('discord-xp')
+// const canvacord = require('canvacord')
+// module.exports = {
+//     name: 'rank',
+//     run: async (client, message, args) => {
+//         const target = message.mentions.users.first() || message.author
+//         let memberTarget = message.guild.members.cache.get(target.id);
+//         const user = await Levels.fetch(target.id, message.guild.id, true);
+//         const neededXp = Levels.xpFor(parseInt(user.level) + 1);
         
-        if (user.length < 1) return message.reply({
-            content: `${process.env.FAILURE_EMOJI} You don't have xp. Send messages to gain some xp!`,
+//         if (user.length < 1) return message.reply({
+//             content: `${process.env.FAILURE_EMOJI} You don't have xp. Send messages to gain some xp!`,
 
-        })
-        const rank = new canvacord.Rank()
-            .setAvatar(memberTarget.user.displayAvatarURL({
-                dynamic: false,
-                format: 'png'
-            }))
-            .setCurrentXP(user.xp)
-            .setLevel(user.level || 0)
-            .setRequiredXP(neededXp)
-            .setRank(user.position)
-            .setStatus('online')
-            .setProgressBar('##278be8', 'COLOR')
-            .setUsername(memberTarget.user.username)
-            .setDiscriminator(memberTarget.user.discriminator)
-            .setBackground("IMAGE", "C:/Users/Pavan/Downloads/maxresdefault.jpg")
+//         })
+//         const rank = new canvacord.Rank()
+//             .setAvatar(memberTarget.user.displayAvatarURL({
+//                 dynamic: false,
+//                 format: 'png'
+//             }))
+//             .setCurrentXP(user.xp)
+//             .setLevel(user.level || 0)
+//             .setRequiredXP(neededXp)
+//             .setRank(user.position)
+//             .setStatus('online')
+//             .setProgressBar('##278be8', 'COLOR')
+//             .setUsername(memberTarget.user.username)
+//             .setDiscriminator(memberTarget.user.discriminator)
+//             .setBackground("IMAGE", "C:/Users/Pavan/Downloads/maxresdefault.jpg")
 
 
-        rank.build()
-            .then(data => {
-                const attachment = new Discord.MessageAttachment(data, "rankcard.png");
-                message.reply({
-                    files: [attachment],
-                    allowedMentions: {
-                        repliedUser: false
-                    }
-                });
-            });
+//         rank.build()
+//             .then(data => {
+//                 const attachment = new Discord.MessageAttachment(data, "rankcard.png");
+//                 message.reply({
+//                     files: [attachment],
+//                     allowedMentions: {
+//                         repliedUser: false
+//                     }
+//                 });
+//             });
 
-    }
-}
+//     }
+// }
