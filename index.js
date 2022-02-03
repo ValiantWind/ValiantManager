88a59c3c89361354da7dab9b35652a5d8778bc5a
ValiantWind <windbhatt@gmail.com>
2022-02-02 20:31:08 -0800

    Attempts to fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const Client = require('./Client.js');
-const config = require('./config.json');
 const express = require('express');
 const app = express();
 
@@ -10,30 +8,32 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+const config = require('./config.json');
+const Discord = require('discord.js')
+const {Client, Intents} = require('discord.js');
+const ClientFile = require('./Client.js');
 
+global.__basedir = __dirname;
 
-const { Collection, Intents } = require('discord.js');
-const intents = new Intents({
-    intents: [
-        "GUILDS",
-        "GUILD_MEMBERS",
-        "GUILD_BANS",
-        "GUILD_EMOJIS_AND_STICKERS",
-        "GUILD_INTEGRATIONS",
-        "GUILD_WEBHOOKS",
-        "GUILD_INVITES",
-        "GUILD_VOICE_STATES",
-        "GUILD_PRESENCES",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS",
-        "GUILD_MESSAGE_TYPING",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGE_TYPING",
-        "GUILD_SCHEDULED_EVENTS",
-    ],
+const client = new Discord.Client(config, {intents: [
+    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILD_MESSAGES, 
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING
+]
 });
-const client = new Client(config, { ws: { intents: intents } });
 //module.exports = client;
 
 require('dotenv').config()
